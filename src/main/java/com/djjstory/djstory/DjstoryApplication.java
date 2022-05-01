ef564fd751d5f02c8c68d7c861fa3ff9bb267f8c
uwhami <uwhami@gmail.com>
2022-05-01 23:50:01 +0900

    git commit

---
+++ src/main/java/com/djjstory/djstory/DjstoryApplication.java
@@ -0,0 +1,13 @@
+package com.djjstory.djstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjstoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djjstory/djstory/DjstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djjstory.djstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
