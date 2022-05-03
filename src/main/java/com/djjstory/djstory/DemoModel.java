395fcccedc8c616913aa646b65e7cffea2d26df7
dajeong <uwhami@gmail.com>
2022-05-03 23:16:39 +0900

    ..

--- src/main/java/com/djjstory/djstory/DemoModel.java
+++ src/main/java/com/djjstory/djstory/DemoModel.java
@@ -1,5 +1,14 @@
 package com.djjstory.djstory;
 
-public class DemoModel {
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
+@Builder
+@RequiredArgsConstructor
+public class DemoModel {
+	
+	@NonNull
+	private String id;
+	
 }
