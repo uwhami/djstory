be2b2cfe679e0250a923e2465dce1146561de41b
dajeong <uwhami@gmail.com>
2022-05-09 23:47:13 +0900

    add test controller

--- src/main/java/com/djjstory/djstory/model/TodoEntity.java
+++ src/main/java/com/djjstory/djstory/model/TodoEntity.java
@@ -1,5 +1,17 @@
 package com.djjstory.djstory.model;
 
-public class TodoEntity {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoEntity {
+	private String id;
+	private String userId;
+	private String title;
+	private boolean done;
 }
