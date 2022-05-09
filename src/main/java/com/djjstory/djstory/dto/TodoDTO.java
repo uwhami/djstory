3b0cc34e2ab7183ea1268456e26290d319d4ea35
dajeong <uwhami@gmail.com>
2022-05-09 23:09:43 +0900

    model entity dto

---
+++ src/main/java/com/djjstory/djstory/dto/TodoDTO.java
@@ -0,0 +1,26 @@
+package com.djjstory.djstory.dto;
+
+import com.djjstory.djstory.model.TodoEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoDTO {
+	
+	private String id;
+	private String title;
+	private boolean done;
+	
+	public TodoDTO(final TodoEntity entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.done = entity.isDone();
+	}
+	
+}
---
+++ src/main/java/com/djjstory/djstory/model/TodoEntity.java
@@ -0,0 +1,5 @@
+package com.djjstory.djstory.model;
+
+public class TodoEntity {
+
+}
