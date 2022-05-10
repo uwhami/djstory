c4bfacd99bc6ae25d4b06d077c84314db1bdb0bc
dajeong <uwhami@gmail.com>
2022-05-10 23:01:28 +0900

    controller getMapping

--- src/main/java/com/djjstory/djstory/controller/TestController.java
+++ src/main/java/com/djjstory/djstory/controller/TestController.java
@@ -1,16 +1,48 @@
 package com.djjstory.djstory.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.djjstory.djstory.dto.ResponseDTO;
+import com.djjstory.djstory.dto.TestRequestBodyDTO;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
 
-	@GetMapping
+	@GetMapping("/testGetMapping")
 	public String testController() {
-		return "Hello World!";
+		return "Hello World! testGetMapping";
+	}
+	
+	@GetMapping("/{id}")
+	public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
+		return "Hello World! ID /" + id;
+	}
+	
+	@GetMapping("/testRequestParam")
+	public String testControllerRequestParam(@RequestParam(required = false) int id) {
+		return "Hello World! id?" + id;
+	}
+	
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello World! testRequestBody id : " + testRequestBodyDTO.getId() + ", message : " + testRequestBodyDTO.getMessage();
+	}
+	
+	@GetMapping("/testResponseBody")
+	public ResponseDTO<String> testControllerResponseBody(){
+		List<String> list = new ArrayList<>();
+		list.add("Hellow World! testResponseBody");
+		ResponseDTO<String> responseDTO = ResponseDTO.<String>builder().data(list).build();
+		return responseDTO;
 	}
 	
 }
---
+++ src/main/java/com/djjstory/djstory/dto/TestRequestBodyDTO.java
@@ -0,0 +1,9 @@
+package com.djjstory.djstory.dto;
+
+import lombok.Data;
+
+@Data
+public class TestRequestBodyDTO {
+	private int id;
+	private String message;
+}
