80d7180507602ee3d2a52b5555f612fd57f56522
dajeong <uwhami@gmail.com>
2022-05-11 23:29:48 +0900

    .

--- src/main/java/com/djjstory/djstory/persistence/TodoRepository.java
+++ src/main/java/com/djjstory/djstory/persistence/TodoRepository.java
@@ -1,5 +1,7 @@
 package com.djjstory.djstory.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.djjstory.djstory.model.TodoEntity;
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String>{
 
+	List<TodoEntity> findByUserId(String userId);
+	
 }
