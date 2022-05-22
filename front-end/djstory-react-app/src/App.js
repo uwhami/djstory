cb951f583e2c881dd1dcd24b6b831aa4b16f226c
dajeong <uwhami@gmail.com>
2022-05-22 22:54:46 +0900

    addTodo

--- front-end/djstory-react-app/src/App.js
+++ front-end/djstory-react-app/src/App.js
@@ -1,34 +1,43 @@
 import React from "react";
 import "./App.css";
 import Todo from "./Todo";
-import { Paper, List } from "@material-ui/core";
+import { Paper, List, Container } from "@material-ui/core";
+import AddTodo from "./AddTodo";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [
-        { id: 0, title: "Hello World 1", done: true },
-        { id: 1, title: "Hello World 2", done: false },
+        { id: "0", title: "Hello World 1", done: true },
+        { id: "1", title: "Hello World 2", done: false },
       ],
     };
   }
 
   render() {
-    // var todoItems = this.state.items.length > 0 && (
-    //   <Paper style={{ margin: 16 }}>
-    //     <List>
-    //       {this.state.items.map((item, idx) => (
-    //         <Todo item={item} key={item.id} />
-    //       ))}
-    //     </List>
-    //   </Paper>
-    // );
-    var todoItems = this.state.items.map((item, idx) => (
-      <Todo item={item} kee={item.id} />
-    ));
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} />
+          ))}
+        </List>
+      </Paper>
+    );
 
-    return <div className="App">{todoItems}</div>;
+    // var todoItems = this.state.items.map((item, idx) => (
+    //   <Todo item={item} key={item.id} />
+    // ));
+
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
   }
 }
 
--- front-end/djstory-react-app/src/Todo.js
+++ front-end/djstory-react-app/src/Todo.js
@@ -6,34 +6,35 @@ class Todo extends React.Component {
     super(props);
     this.state = { item: props.item };
   }
+
   render() {
     const item = this.state.item;
 
     return (
-        <div className="Todo">
-          <input
-            type="checkbox"
-            id={this.state.item.id}
-            name={this.state.item.id}
-            checked={this.state.item.done}
-          />
-          <label id={this.state.item.id}>{this.state.item.title}</label>
-        </div>
+      // <div className="Todo">
+      //   <input
+      //     type="checkbox"
+      //     id={this.state.item.id}
+      //     name={this.state.item.id}
+      //     checked={this.state.item.done}
+      //   />
+      //   <label id={this.state.item.id}>{this.state.item.title}</label>
+      // </div>
 
-    //   <ListItem>
-    //     <Checkbox checked={item.done} />
-    //     <ListItemText>
-    //       <InputBase
-    //         inputProps={{ "aria-label": "naked" }}
-    //         type="text"
-    //         id={item.id}
-    //         name={item.id}
-    //         value={item.title}
-    //         multiline={true}
-    //         fullWidth={true}
-    //       />
-    //     </ListItemText>
-    //   </ListItem>
+      <ListItem>
+        <Checkbox checked={item.done} />
+        <ListItemText>
+          <InputBase
+            inputProps={{ "aria-label": "naked" }}
+            type="text"
+            id={item.id}
+            name={item.id}
+            value={item.title}
+            multiline={true}
+            fullWidth={true}
+          />
+        </ListItemText>
+      </ListItem>
     );
   }
 }
