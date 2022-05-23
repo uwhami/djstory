4d1280291ade178407cd186edad0037875fa4a93
dajeong <uwhami@gmail.com>
2022-05-23 23:37:00 +0900

    changeEvent

--- front-end/djstory-react-app/src/AddTodo.js
+++ front-end/djstory-react-app/src/AddTodo.js
@@ -4,18 +4,36 @@ import { TextField, Paper, Button, Grid } from "@material-ui/core";
 class AddTodo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { item: { title: "." } };
+    this.state = { item: { title: "" } };
   }
+
+  onInputChange = (e) => {
+    const thisItem = this.state.item;
+    thisItem.title = e.target.value;
+    this.setState({ item: thisItem });
+  };
+
+  onClickEvent = (e) => {};
 
   render() {
     return (
       <Paper style={{ margin: 16, padding: 16 }}>
         <Grid container>
-          <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-            <TextField placeholder="Add Todo hear" fullWidth />
+          <Grid xs={11} item style={{ paddingRight: 16 }}>
+            <TextField
+              placeholder="Add Todo hear"
+              fullWidth
+              onChange={this.onInputChange}
+              value={this.state.item.title}
+            />
           </Grid>
-          <Grid xs={1} md={1} item>
-            <Button fullWidth color="secondary" variant="outlined">
+          <Grid xs={1} item>
+            <Button
+              fullWidth
+              color="secondary"
+              variant="outlined"
+              onClick={this.onClickEvent}
+            >
               +
             </Button>
           </Grid>
