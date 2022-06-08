159e27a57c58c414bf0cc81e304ce861ace3c7a4
dajeong <uwhami@gmail.com>
2022-06-09 00:21:31 +0900

    frontend login page

--- front-end/djstory-react-app/src/service/ApiService.js
+++ front-end/djstory-react-app/src/service/ApiService.js
@@ -1,13 +1,20 @@
 import { API_BASE_URL } from "../app-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
+  let headers = new Headers({ "Content-Type": "application/json" });
+
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  if (accessToken && accessToken !== null) {
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
+
   let options = {
-    headers: new Headers({
-      "Content-Type": "application/json",
-    }),
+    headers: headers,
     url: API_BASE_URL + api,
     method: method,
   };
+
   if (request) {
     //GET method
     options.body = JSON.stringify(request);
@@ -33,8 +40,14 @@ export function call(api, method, request) {
 }
 
 export function signin(userDTO) {
-  return call("/auth/signin", "POST", userDTO).then((response) => {
-    console.log("response : ", response);
-    alert("로그인 토큰: " + response.token);
-  });
+  return call("/auth/signin", "POST", userDTO)
+    .then((response) => {
+      if (response.token) {
+        localStorage.setItem("ACCESS_TOKEN", response.token);
+        window.location.href = "/";
+      }
+    })
+    .catch((error) => {
+      alert("아이디와 비밀번호를 확인해주세요.");
+    });
 }
